b85287a499ae9cdbd5a34ffb954cf179fd8e8e8d
Julian Romero <juliandavirc@gmail.com>
2024-03-03 19:33:20 -0500

    QUERY ANALYSIS 1

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-## RestApi Python/SQL Database + Docker container solution
+# RestApi Python/SQL Database + Docker container solution
 
-## Endpoint to load batch 1 up to 1000 transactions (HTTP Post Request)
+Endpoint to load batch 1 up to 1000 transactions (HTTP Post Request)
 
 Load dimension data (departments & jobs)
-http://127.0.0.1:5000/load/dims
+`http://127.0.0.1:5000/load/dims`
 
 Load hired employees data 
-http://127.0.0.1:5000/load/hired_employees
+`http://127.0.0.1:5000/load/hired_employees`
+
+
+
+# To call avro backup feature: Read package from spark/avro version
+
+    PUT `/backupAvro`
+
+        os.system("/opt/homebrew/Cellar/apache-spark/3.5.1/bin/spark-submit --packages org.apache.spark:spark-avro_2.12:3.5.1 compressAvro.py &")
 
--- app.py
+++ app.py
@@ -85,6 +85,23 @@ def query_hiredemp():
           
         connection.commit()
 
+@app.get("/query/req1")
+def query_req1():               
+
+    # Queries Hired Employees
+    query1 = "select department, job, (case when quarter = 1 then conteo else 0 end) Q1, (case when quarter = 2 then conteo else 0 end) Q2, (case when quarter = 3 then conteo else 0 end) Q3, (case when quarter = 4 then conteo else 0 end) Q4 from ( SELECT COALESCE(d.department,'Undefined') as department, COALESCE(j.job,'Undefined') as Job, EXTRACT(QUARTER FROM to_date(e.field2,'YYYY-MM-DD')) as quarter, count(*) conteo FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id LEFT JOIN public.jobs j ON j.id = e.job_id WHERE DATE_PART('Year', to_date(e.field2,'YYYY-MM-DD')) = '2021' group by 1,2,3 )r order by department,job;"
+
+    ## Query1: Number of employees hired for each job and department in 2021 divided by quarter. The table must be ordered alphabetically by department and job.
+    with connection:
+        with connection.cursor() as cursor:            
+            cursor.execute(query1)    
+            columns = [column[0] for column in cursor.description]
+            objdata = []          
+            for row in cursor.fetchall():
+                objdata.append(dict(zip(columns, row)))            
+                
+    return {"Result": objdata,}, 200            
+
 @app.get("/query/backup")
 def query_bkp():               
 
