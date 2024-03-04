48dd66e8b9a9660379e36a1436cdf3241146aa24
Julian Romero <juliandavirc@gmail.com>
2024-03-04 11:23:05 -0500

    Queries analysis

--- app.py
+++ app.py
@@ -63,14 +63,14 @@ def query_dept():
 def query_hiredemp():               
 
     #load df Hired Employees
-    data_hiredemp = pd.read_csv(path_hiredemp, header=None, names=['id', 'nameemp', 'field2', 'department_id', 'job_id'])       
+    data_hiredemp = pd.read_csv(path_hiredemp, header=None, names=['id', 'nameemp', 'hire_date', 'department_id', 'job_id'])       
     data_hiredemp=data_hiredemp.fillna(0) 
-    data_hiredemp['field2'] = data_hiredemp['field2'].replace(0,'1900-01-01T00:00:00Z')   
+    data_hiredemp['hire_date'] = data_hiredemp['hire_date'].replace(0,'1900-01-01T00:00:00Z')   
 
     # Queries Hired Employees
-    ddl_table_hired_emp = "CREATE TABLE IF NOT EXISTS hired_employees(id int, nameemp varchar(200), field2 varchar(100), department_id int, job_id int);"
+    ddl_table_hired_emp = "CREATE TABLE IF NOT EXISTS hired_employees(id int, nameemp varchar(200), hire_date varchar(100), department_id int, job_id int);"
     dml_delete_hired_emp = "TRUNCATE TABLE hired_employees;"
-    dml_insert_hired_emp = "INSERT INTO hired_employees (id, nameemp, field2, department_id, job_id) VALUES (%s,%s,%s,%s,%s);"
+    dml_insert_hired_emp = "INSERT INTO hired_employees (id, nameemp, hire_date, department_id, job_id) VALUES (%s,%s,%s,%s,%s);"
 
     ## INSERT
     with connection:
@@ -79,9 +79,9 @@ def query_hiredemp():
             cursor.execute(ddl_table_hired_emp)
             cursor.execute(dml_delete_hired_emp) 
             #cursor.execute("select count(*) from hired_employees")              
-            for index, row in data_hiredemp.iloc[0:1000,].iterrows():
-            #for index, row in data_hiredemp.iterrows():
-                cursor.execute(dml_insert_hired_emp, (row.id, row.nameemp, row.field2, row.department_id, row.job_id),)
+            #for index, row in data_hiredemp.iloc[0:2000,].iterrows():
+            for index, row in data_hiredemp.iterrows():
+                cursor.execute(dml_insert_hired_emp, (row.id, row.nameemp, row.hire_date, row.department_id, row.job_id),)
           
         connection.commit()
 
@@ -89,8 +89,9 @@ def query_hiredemp():
 def query_req1():               
 
     # Queries Hired Employees
-    query1 = "select department, job, (case when quarter = 1 then conteo else 0 end) Q1, (case when quarter = 2 then conteo else 0 end) Q2, (case when quarter = 3 then conteo else 0 end) Q3, (case when quarter = 4 then conteo else 0 end) Q4 from ( SELECT COALESCE(d.department,'Undefined') as department, COALESCE(j.job,'Undefined') as Job, EXTRACT(QUARTER FROM to_date(e.field2,'YYYY-MM-DD')) as quarter, count(*) conteo FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id LEFT JOIN public.jobs j ON j.id = e.job_id WHERE DATE_PART('Year', to_date(e.field2,'YYYY-MM-DD')) = '2021' group by 1,2,3 )r order by department,job;"
-
+    #query1 = "select department, job, (case when quarter = 1 then conteo else 0 end) Q1, (case when quarter = 2 then conteo else 0 end) Q2, (case when quarter = 3 then conteo else 0 end) Q3, (case when quarter = 4 then conteo else 0 end) Q4 from ( SELECT COALESCE(d.department,'Undefined') as department, COALESCE(j.job,'Undefined') as Job, EXTRACT(QUARTER FROM to_date(e.hire_date,'YYYY-MM-DD')) as quarter, count(*) conteo FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id LEFT JOIN public.jobs j ON j.id = e.job_id WHERE DATE_PART('Year', to_date(e.hire_date,'YYYY-MM-DD')) = '2021' group by 1,2,3 )r order by department,job;"
+    query1 = "With tablea as (SELECT COALESCE(d.department, 'Undefined') AS department, COALESCE(j.job, 'Undefined') AS Job, EXTRACT(QUARTER FROM to_date(e.hire_date, 'YYYY-MM-DD')) AS QUARTER, count(*) conteo FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id LEFT JOIN public.jobs j ON j.id = e.job_id WHERE DATE_PART('Year', to_date(e.hire_date, 'YYYY-MM-DD')) = '2021' GROUP BY 1,   2,   3)SELECT department,job, (CASE WHEN QUARTER = 1 THEN conteo ELSE 0 END) Q1,  (CASE WHEN QUARTER = 2 THEN conteo ELSE 0 END) Q2, (CASE WHEN QUARTER = 3 THEN conteo ELSE 0 END) Q3, (CASE WHEN QUARTER = 4 THEN conteo ELSE 0 END) Q4 FROM  tablea ORDER BY department,job;"
+    
     ## Query1: Number of employees hired for each job and department in 2021 divided by quarter. The table must be ordered alphabetically by department and job.
     with connection:
         with connection.cursor() as cursor:            
@@ -100,7 +101,28 @@ def query_req1():
             for row in cursor.fetchall():
                 objdata.append(dict(zip(columns, row)))            
                 
-    return {"Result": objdata,}, 200            
+    return {"Result": objdata,}, 200      
+
+
+@app.get("/query/req2")
+def query_req2():               
+
+    # Queries Number employees by department with count > avg departments in 2021    
+    query2 = "WITH tableb as (SELECT d.department, count(*) hired FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id WHERE DATE_PART('Year', to_date(e.hire_date,'YYYY-MM-DD')) = '2021' group by 1 ) SELECT d.id, COALESCE(d.department ,'Undefined') department ,count(*) hired FROM public.hired_employees e LEFT JOIN public.departments d ON d.id = e.department_id group by 1,2 having count(*) > (select avg(hired)::float from tableb) order by 3 desc;"
+
+    ## Query2: List of ids, name and number of employees hired of each department that hired more employees than the mean of employees
+    #  hired in 2021 for all the departments, ordered by the number of employees hired (descending).
+    with connection:
+        with connection.cursor() as cursor:
+            #Load Deparment Hired Emp
+            cursor.execute(query2)            
+            columns = [column[0] for column in cursor.description]
+            objdata = []          
+            for row in cursor.fetchall():
+                objdata.append(dict(zip(columns, row)))    
+            
+    return {"Result": objdata,}, 200
+
 
 @app.get("/query/backup")
 def query_bkp():               
