083072664bac3ef5e64697230f8ae96918f16e62
Julian Romero <juliandavirc@gmail.com>
2024-03-04 10:14:51 -0500

    Backup Avro Dept, Jobs, Hired_Employees

--- compressAvro.py
+++ compressAvro.py
@@ -3,9 +3,24 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Compress").master("local").getOrCreate()
 
-databackup = pd.read_csv('data/backup/csv/departments.gz', index_col=0)
-df = spark.createDataFrame(databackup)
+#Departments
+databackup_dep = pd.read_csv('data/backup/csv/departments.gz', index_col=0)
+df_dept = spark.createDataFrame(databackup_dep)
 
 # Save as Avro
-df.write.format('avro').mode('overwrite').save('data/backup/avro/departmentsAvro')
+df_dept.write.format('avro').mode('overwrite').save('data/backup/avro/departmentsAvro')
+
+#Jobs
+databackup_jobs = pd.read_csv('data/backup/csv/jobs.gz', index_col=0)
+df_jobs = spark.createDataFrame(databackup_jobs)
+
+# Save as Avro
+df_jobs.write.format('avro').mode('overwrite').save('data/backup/avro/jobsAvro')
+
+#Hired Employees
+databackup_empl = pd.read_csv('data/backup/csv/hired_employees.gz', index_col=0)
+df_empl = spark.createDataFrame(databackup_empl)
+
+# Save as Avro
+df_empl.write.format('avro').mode('overwrite').save('data/backup/avro/hired_employeesAvro')
 
